66e9258a6ad39cf576c3f606820b88542f44a132
Etienne Bélanger <etienne.belanger@gmail.com>
2018-03-31 13:57:10 -0400

    Add test cases for GET /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,13 +1,16 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+    _id: new ObjectId(),
     text: 'First test todo'
 },
 {
+    _id: new ObjectId(),
     text: 'Second test todo'
 }];
 
@@ -68,5 +71,34 @@ describe('POST /todos', () =>{
                 })
                 .end(done);
         });
+    });
+});
+
+describe('GET /todos/:id', ()=>{
+    it('should return todo doc', (done) =>{
+        request(app)
+            .get(`/todos/${todos[0]._id.toHexString()}`)
+            .expect(200)
+            .expect((res) =>{
+                expect(res.body.todo.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found',(done)=>{
+        var fakeid= new ObjectId();
+        request(app)
+        .get(`/todos/${fakeid.toHexString()}`)
+        .expect(404)
+        .end(done)
+    });
+
+    it('should return 404 for non-object ids',(done) =>{
+        var fakeid = '123456645798';
+
+        request(app)
+        .get(`/todos/${fakeid}`)
+        .expect(404)
+        .end(done)
     });
 });
