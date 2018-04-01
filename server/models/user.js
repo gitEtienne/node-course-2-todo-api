16245d78d580b1dc09af8646bd536bef4becdb1f
Etienne Bélanger <etienne.belanger@gmail.com>
2018-04-01 15:14:13 -0400

    Add DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -95,6 +95,16 @@ UserSchema.statics.findByCredential = function(email, password) {
     });
 };
 
+UserSchema.methods.removeToken = function(token) {
+    var user = this;
+
+    return user.update({
+        $pull: {
+            tokens: {token}
+        }
+    });
+}
+
 UserSchema.pre('save', function (next){
     var user = this;
 
